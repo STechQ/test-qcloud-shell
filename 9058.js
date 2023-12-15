1f7ccf2c4679efd5007374f90c5d0b7634a1b435
STechQ <stech.quick@yandex.com>
2023-12-15 09:32:29 +0000

    deploy for test

--- 9058.js
+++ 9058.js
@@ -1,2 +1,2 @@
 "use strict";(this.webpackChunkqcloud_shell=this.webpackChunkqcloud_shell||[]).push([[9058,9508],{50419:(o,s,c)=>{c.d(s,{L:()=>l});const l=Symbol.for("INotification")},19508:(o,s,c)=>{c.d(s,{h:()=>l});const l=Symbol.for("IApplicationUseCase")}}]);
-//# sourceMappingURL=9058.js.map?ver=0.0.285_1702586383672
+//# sourceMappingURL=9058.js.map?ver=0.0.285_1702632606231
