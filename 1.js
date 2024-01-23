f03222a9d4d2b1f8bd53c838f1ee063bb919cdf9
STechQ <stech.quick@yandex.com>
2024-01-23 13:24:20 +0000

    deploy for test

--- 1.js
+++ 1.js
@@ -1,2 +1,2 @@
 "use strict";(this.webpackChunkqcloud_shell=this.webpackChunkqcloud_shell||[]).push([[1,2525],{32525:(s,c)=>{c.Z=(s,c)=>{const t=s.__vccOpts||s;for(const[s,h]of c)t[s]=h;return t}}}]);
-//# sourceMappingURL=1.js.map?ver=0.0.296_1705647348730
+//# sourceMappingURL=1.js.map?ver=0.0.296_1706016063694
