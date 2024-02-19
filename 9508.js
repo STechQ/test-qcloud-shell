a06dd6f738e2d4d0b31f6d8ad4fd04922c78945b
STechQ <stech.quick@yandex.com>
2024-02-19 11:34:58 +0000

    deploy for test

--- 9508.js
+++ 9508.js
@@ -1,2 +1,2 @@
 "use strict";(this.webpackChunkqcloud_shell=this.webpackChunkqcloud_shell||[]).push([[9508],{19508:(s,h,l)=>{l.d(h,{h:()=>c});const c=Symbol.for("IApplicationUseCase")}}]);
-//# sourceMappingURL=9508.js.map?ver=0.0.299_1708328963601
+//# sourceMappingURL=9508.js.map?ver=0.0.299_1708342310774
