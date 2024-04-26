0ba2b3ce343e6208a7567704524c82b1083026bb
STechQ <stech.quick@yandex.com>
2024-04-26 13:39:49 +0000

    deploy for test

--- 8613.js
+++ 8613.js
@@ -1,2 +1,2 @@
 "use strict";(this.webpackChunkqcloud_shell=this.webpackChunkqcloud_shell||[]).push([[8613],{88613:(l,s,e)=>{e.d(s,{J:()=>h});const h=Symbol.for("IViewModel")}}]);
-//# sourceMappingURL=8613.js.map?ver=0.0.314_1713967654769
+//# sourceMappingURL=8613.js.map?ver=0.0.314_1714138609251
--- qui-lp/common/qshell/src/interfaces/construction/IIntegrationsManager.d.ts
+++
@@ -1,6 +0,0 @@
-import { IContextItem } from "../../../../shrimp/context";
-export declare const IntegrationsManagerContextName: string;
-export interface IIntegrationsManager extends IContextItem {
-    customIntegrationEventTrigger(name: string, params: any[]): void;
-}
-//# sourceMappingURL=IIntegrationsManager.d.ts.map
