3d3814bf37e57c74d02961db14e053cb4be990c7
STechQ <stech.quick@yandex.com>
2024-05-02 11:40:33 +0000

    deploy for test

--- 2525.js
+++ 2525.js
@@ -1,2 +1,2 @@
 "use strict";(this.webpackChunkqcloud_shell=this.webpackChunkqcloud_shell||[]).push([[2525,1],{32525:(s,c)=>{c.Z=(s,c)=>{const t=s.__vccOpts||s;for(const[s,h]of c)t[s]=h;return t}}}]);
-//# sourceMappingURL=2525.js.map?ver=0.0.314_1714647955860
+//# sourceMappingURL=2525.js.map?ver=0.0.314_1714649852497
