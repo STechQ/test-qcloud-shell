ef6a38ece49eb97a8b47839ef97f7a04c2c5a657
STechQ <stech.quick@yandex.com>
2024-06-03 09:01:38 +0000

    deploy for test

--- 8613.js
+++ 8613.js
@@ -1,2 +1,2 @@
 "use strict";(this.webpackChunkqcloud_shell=this.webpackChunkqcloud_shell||[]).push([[8613],{88613:(l,s,e)=>{e.d(s,{J:()=>h});const h=Symbol.for("IViewModel")}}]);
-//# sourceMappingURL=8613.js.map?ver=0.0.319_1717146860939
+//# sourceMappingURL=8613.js.map?ver=0.0.319_1717405089305
