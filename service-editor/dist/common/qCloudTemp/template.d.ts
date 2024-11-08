f7dd85f853b940acea31ed927e41f758eeb7f917
STechQ <stech.quick@yandex.com>
2024-11-08 14:20:07 +0000

    deploy for test

--- service-editor/dist/common/qCloudTemp/template.d.ts
+++ service-editor/dist/common/qCloudTemp/template.d.ts
@@ -6,6 +6,7 @@ export interface ITemplateInfo {
     type: TemplateType;
     priority?: number;
     image?: string;
+    targetObjectIds?: Array<string>;
 }
 export interface IPageTemplate extends ITemplateInfo {
     type: "page";
