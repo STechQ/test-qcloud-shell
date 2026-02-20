3adcab5455a849eac952599d6c3377dc3bfcbd01
STechQ <stech.quick@yandex.com>
2026-02-20 16:14:23 +0000

    deploy for test

--- soapservice-editorv2/dist/jobs/src/domain/model/IExportItemBody.d.ts
+++ soapservice-editorv2/dist/jobs/src/domain/model/IExportItemBody.d.ts
@@ -1,5 +1,6 @@
 import { IExportItem } from "./IExportJob";
 export interface IExportItemBody extends IExportItem {
     body: string;
+    originalName?: string;
 }
 //# sourceMappingURL=IExportItemBody.d.ts.map
